dec61e92d36d1d8045f01bb33fe7869f879c47a8
JM97142 <jeanmichel.taret@gmail.com>
2025-08-14 11:01:22 +0200

    update UI

--- app/src/main/java/com/example/joiefull_p12/ui/screens/ProductDetailScreen.kt
+++ app/src/main/java/com/example/joiefull_p12/ui/screens/ProductDetailScreen.kt
@@ -1,12 +1,45 @@
 package com.example.joiefull_p12.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 import com.example.joiefull_p12.data.models.ProductModel
 
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun ProductDetailScreen(
     productModel: ProductModel,
     navController: NavController
 ) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        GlideImage(
+            model = productModel.imageUrl,
+            contentDescription = productModel.title,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(430.dp)
+                .padding(vertical = 16.dp)
+        )
+        Button(
+            onClick = {
+                navController.popBackStack()
+            }) {
+            Text("Back")
+        }
+    }
 }
--- app/src/main/java/com/example/joiefull_p12/ui/screens/ProductListScreen.kt
+++ app/src/main/java/com/example/joiefull_p12/ui/screens/ProductListScreen.kt
@@ -1,10 +1,15 @@
 package com.example.joiefull_p12.ui.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
@@ -19,6 +24,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextDecoration
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 
@@ -60,25 +73,72 @@ fun ProductListScreen(
                         Card(
                             modifier = Modifier
                                 .padding(8.dp)
-                                .width(150.dp)
+                                .width(198.dp)
                                 .clickable {
                                     navController.navigate("detail/${product.id}")
                                 },
                             elevation = CardDefaults.cardElevation(4.dp)
                         ) {
-                            GlideImage(
-                                model = product.imageUrl,
-                                contentDescription = product.description,
-                                contentScale = ContentScale.Crop,
+                            Box(
                                 modifier = Modifier
-                                    .height(150.dp)
+                                    .height(198.dp)
                                     .fillMaxWidth()
-                            )
+                                    .background(
+                                        color = Color.White,
+                                        shape = RoundedCornerShape(80)
+                                    )
+                            ) {
+                                GlideImage(
+                                    model = product.imageUrl,
+                                    contentDescription = product.description,
+                                    contentScale = ContentScale.Crop
+                                )
+                                Row(
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    modifier = Modifier
+                                        .align(Alignment.BottomEnd)
+                                        .padding(8.dp)
+                                        .background(
+                                            color = Color.White,
+                                            shape = RoundedCornerShape(50)
+                                        )
+                                        .padding(horizontal = 8.dp, vertical = 4.dp)
+                                    ) {
+                                    Icon(
+                                        imageVector = Icons.Default.FavoriteBorder,
+                                        contentDescription = "Likes",
+                                        tint = Color.Black,
+                                        modifier = Modifier.size(16.dp)
+                                    )
+                                    Text(
+                                        text = product.rating.toString(),
+                                        style = MaterialTheme.typography.bodySmall,
+                                        color = Color.Black
+                                    )
+                                }
+                            }
                             Text(
                                 text = product.title,
-                                style = MaterialTheme.typography.bodyMedium,
+                                style = MaterialTheme.typography.titleMedium,
                                 modifier = Modifier.padding(8.dp)
                             )
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(8.dp),
+                                horizontalArrangement = Arrangement.SpaceBetween
+                            ) {
+                                Text(
+                                    text = "${ product.price }€",
+                                    style = MaterialTheme.typography.titleSmall
+                                )
+                                Text(
+                                    text = "${ product.oldPrice }€",
+                                    style = MaterialTheme.typography.titleSmall.copy(
+                                        textDecoration = TextDecoration.LineThrough
+                                    )
+                                )
+                            }
                         }
                     }
                 }
