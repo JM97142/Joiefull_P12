da759c29cb5c04ae3a4580d57538eaee659b110c
JM97142 <jeanmichel.taret@gmail.com>
2025-08-16 03:50:35 +0200

    add rating system

--- app/src/main/java/com/example/joiefull_p12/ui/components/ProductReview.kt
+++ app/src/main/java/com/example/joiefull_p12/ui/components/ProductReview.kt
@@ -1,12 +1,23 @@
 package com.example.joiefull_p12.ui.components
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
@@ -22,5 +33,22 @@ fun ProductReview() {
                 .size(36.dp)
                 .clip(CircleShape)
         )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        var selectedStars by remember { mutableStateOf(0) }
+
+        Row {
+            repeat(5) { index ->
+                Icon(
+                    Icons.Default.Star,
+                    contentDescription = null,
+                    tint = if (index < selectedStars) Color(0xFFFF9800) else Color.Gray,
+                    modifier = Modifier
+                        .size(28.dp)
+                        .clickable { selectedStars = index + 1 }
+                )
+            }
+        }
     }
 }
