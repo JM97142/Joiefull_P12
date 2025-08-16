bbfb1ee78460e050d01f64927552a32944acad71
JM97142 <jeanmichel.taret@gmail.com>
2025-08-16 03:30:13 +0200

    add productReview

--- app/src/main/java/com/example/joiefull_p12/ui/components/ProductReview.kt
+++ app/src/main/java/com/example/joiefull_p12/ui/components/ProductReview.kt
@@ -1,6 +1,26 @@
 package com.example.joiefull_p12.ui.components
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
-fun ProductReview() { }
+fun ProductReview() {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        GlideImage(
+            model = "https://randomuser.me/api/portraits/women/44.jpg", // Avatar fictif
+            contentDescription = "Avatar",
+            modifier = Modifier
+                .size(36.dp)
+                .clip(CircleShape)
+        )
+    }
+}
